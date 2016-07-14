7ed0482e33432f3742259f04f8ab2d9f609d0d7b
kirethi7 <bkirethiga@gmail.com>
2016-07-14 18:10:53 +0530

    Create string.java

---
+++ string.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner.*;
+class string
+{
+public static void main(String args[])
+{
+string str="";
+string rev="";
+Scanner sc=new Scanner(System.in);
+str=sc.nextLine();
+rev=strrev(str);
+System.out.println(rev);
+}
+}

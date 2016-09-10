b945e53b9351117188e02a098d15d418d9bfacc9
kirethi7 <bkirethiga@gmail.com>
2016-09-10 19:00:42 +0530

    Update and rename asciival.java to val.java

--- val.java
+++ val.java
@@ -1,14 +1,14 @@
 import java.io.*;
 import java.util.*;
-public class Ascii
+public class fascii
 {
   public static void main(String[] args) 
   {
 		Scanner sc=new Scanner(System.in);
 		for(int i=28;i<=128;i++)
 		{
-			char a=(char) i;
-			System.out.print(a+" ");
+			char cd=(char) i;
+			System.out.print(cd+" ");
 		}
 	sc.close();
 	}

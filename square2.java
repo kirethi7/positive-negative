5bf720e87bafe7a5f13d7847ad990d0eedf3358c
kirethi7 <bkirethiga@gmail.com>
2016-09-18 22:45:02 +0530

    Update square2.java

--- square2.java
+++ square2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-class Square {
+class Square2 {
   public static void main(String[] args) {
     Scanner s=new Scanner(System.in);
     System.out.println("Enter the 4 points: ");

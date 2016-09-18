affe7cce5a9e27df64430cb3f188cb78275a54ad
kirethi7 <bkirethiga@gmail.com>
2016-09-18 21:51:31 +0530

    Create repeatarray.java

---
+++ repeatarray.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.util.Set;
+
+public class rept {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String string=sc.nextLine();
+
+		char[] chars = string.toCharArray();
+		Set<Character> charSet = new LinkedHashSet<Character>();
+		for (char c : chars) {
+		    charSet.add(c);
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (Character character : charSet) {
+		    sb.append(character);
+		}
+		System.out.println(sb.toString());
+		sc.close();
+	}
+	
+
+}
